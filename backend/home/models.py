23e35bb25dbfd913fc0491e28bb315f19d5f1ebe
crowdbotics-apps <team@crowdbotics.com>
2024-07-25 17:04:09 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Hvdgd(models.Model):
+    "Generated Model"
+    ncvsh = models.EmailField(
+        max_length=254,
+    )
+    twyux = models.FloatField()
--- backend/manage.py
+++ backend/manage.py
@@ -5,9 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "tctest2_48858.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tctest2_48858.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
